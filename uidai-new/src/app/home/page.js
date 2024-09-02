1a435f2368ce293f75106899e5469a0c603497ee
Dhanraj Rateria <dhanrajrateria@gmail.com>
2024-09-02 07:56:04 +0530

    frontend

--- uidai-new/src/app/home/page.js
+++ uidai-new/src/app/home/page.js
@@ -3,6 +3,7 @@
 import React, { useEffect } from 'react';
 import Image from 'next/image';
 
+
 const HomePage = () => {
   // Add animation to background or content here if necessary
   useEffect(() => {
